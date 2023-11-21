6c9a75538975d2d0b798dab791fc0808d277c473
김정아 <92552047+squidjiny@users.noreply.github.com>
2023-11-21 12:58:40 +0900

    악

--- src/main/java/com/example/TodoProject/config/SwaggerConfig.java
+++ src/main/java/com/example/TodoProject/config/SwaggerConfig.java
@@ -5,19 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        Server serverLocal = new Server("local", "http://localhost:8080", "for local usage", Collections.emptyList(), Collections.emptyList());
+        Server testServer = new Server("test", "https://jeonga:5154", "for testing", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(false)
                 .select()
